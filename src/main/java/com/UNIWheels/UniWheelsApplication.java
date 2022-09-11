094e79dd2f5a99f545a26091f123b6652ee516e4
Jose Gamboa <jose.gamboa@mail.escuelaing.edu.co>
2022-09-11 18:20:32 -0500

    Start project

---
+++ src/main/java/com/UNIWheels/UniWheelsApplication.java
@@ -0,0 +1,13 @@
+package com.UNIWheels;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniWheelsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniWheelsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/UNIWheels/UniWheelsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.UNIWheels;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniWheelsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
