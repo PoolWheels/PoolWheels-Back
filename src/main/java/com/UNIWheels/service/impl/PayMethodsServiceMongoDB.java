69219a9b1f92f8e071ca0ced86116d50aee16f6d
eduardoospina <eduardo.ospina@mail.escuelaing.edu.co>
2022-09-12 20:23:01 -0500

    PayMethodService implementacion DB

--- src/main/java/com/UNIWheels/service/impl/PayMethodsServiceMongoDB.java
+++ src/main/java/com/UNIWheels/service/impl/PayMethodsServiceMongoDB.java
@@ -3,40 +3,46 @@ package com.UNIWheels.service.impl;
 import com.UNIWheels.entities.PayMethods;
 import com.UNIWheels.repository.PayMethodsRepository;
 import com.UNIWheels.service.PayMethodsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+
+@Service
 public class PayMethodsServiceMongoDB implements PayMethodsService {
 
     private final PayMethodsRepository paymethodsRepository;
 
-    public PayMethodsServiceMongoDB(PayMethodsRepository paymethodsRepository) {
+    public PayMethodsServiceMongoDB(@Autowired PayMethodsRepository paymethodsRepository) {
         this.paymethodsRepository = paymethodsRepository;
     }
 
 
     @Override
-    public PayMethods create(PayMethods user) {
-        return null;
+    public PayMethods create(PayMethods paymethod) {
+        paymethodsRepository.save(paymethod);
+        return paymethod;
     }
 
     @Override
     public PayMethods findById(String id) {
-        return null;
+        return paymethodsRepository.findById(id).get();
     }
 
     @Override
     public List<PayMethods> getAll() {
-        return null;
+        return paymethodsRepository.findAll();
     }
 
     @Override
     public void deleteById(String id) {
-
+        paymethodsRepository.deleteById(id);
     }
 
     @Override
-    public PayMethods update(PayMethods user, String userId) {
-        return null;
+    public PayMethods update(PayMethods paymethod, String userId) {
+        paymethodsRepository.save(paymethod);
+        return paymethodsRepository.findById(userId).get();
     }
 }
