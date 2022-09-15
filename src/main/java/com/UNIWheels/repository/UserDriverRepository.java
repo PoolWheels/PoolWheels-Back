ca6b33a7b171509030c8baad43cf77d8e58396d6
Zuly Vargas <zuly.vargas@mail.escuelaing.edu.co>
2022-09-15 00:47:39 -0500

    Final JWT

--- src/main/java/com/UNIWheels/repository/UserDriverRepository.java
+++ src/main/java/com/UNIWheels/repository/UserDriverRepository.java
@@ -1,9 +1,14 @@
 package com.UNIWheels.repository;
 
+import com.UNIWheels.entities.User;
 import com.UNIWheels.entities.UserDriver;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.Optional;
+
 public interface UserDriverRepository extends MongoRepository<UserDriver,String> {
+
+    Optional<User> findByEmail(String email);
 
 
 
--- src/main/java/com/UNIWheels/repository/UserTravelerRepository.java
+++ src/main/java/com/UNIWheels/repository/UserTravelerRepository.java
@@ -1,7 +1,12 @@
 package com.UNIWheels.repository;
 
+import com.UNIWheels.entities.User;
 import com.UNIWheels.entities.UserTraveler;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.Optional;
+
 public interface UserTravelerRepository extends MongoRepository<UserTraveler,String> {
+    Optional<User> findByEmail(String email);
+
 }
