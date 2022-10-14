041dc477154cdbe51cf84a184c8e6a655221d6b4
Zuly Vargas <zuly.vargas@mail.escuelaing.edu.co>
2022-10-14 11:06:29 -0500

    ISSUE 3: Enum in english

--- src/main/java/com/UNIWheels/enums/enumPayMethod.java
+++ src/main/java/com/UNIWheels/enums/enumPayMethod.java
@@ -2,5 +2,5 @@ package com.UNIWheels.enums;
 
 // Creating an enum called enumPayMethods with three values: Debito, Credito, CarteraVirtual.
 public enum enumPayMethod {
-    Debito, Credito, CarteraVirtual;
+    Debit, Credit, VirtualWallet;
 }
